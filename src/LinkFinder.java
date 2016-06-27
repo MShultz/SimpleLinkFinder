e8ac23addf0c69efd52e39961dbc43bed2fcd55f
MShultz <mshultz@student.neumont.edu>
2016-06-27 16:38:42 -0600

    Initial commit of Simple Link Finder Lab

---
+++ src/LinkFinder.java
@@ -0,0 +1,13 @@
+import java.io.InputStream;
+import java.util.Iterator;
+
+
+public class LinkFinder {
+	  public void processPage(InputStream in) {
+
+	     }
+
+	     public Iterator<String> getLinks() {
+
+	     }
+}
