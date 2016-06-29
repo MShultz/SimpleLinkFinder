c1d0c598d1cb1fe6b0271580b084a0c9e5dc201a
MShultz <mshultz@student.neumont.edu>
2016-06-29 14:49:59 -0600

    Completed LinkFinder class with working ReGex - JUnit Test still fails

--- src/LinkFinder.java
+++ src/LinkFinder.java
@@ -1,13 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-
+//Question: I can name the group, but how do I call it? It wouldn't know about
 public class LinkFinder {
-	  public void processPage(InputStream in) {
+	ArrayList<String> linkList = new ArrayList<String>();
 
-	     }
+	public void processPage(InputStream in) {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		String pattern = "<\\s*[Aa]\\s+[Hh][Rr][Ee][Ff]\\s*=\\s*\"(?<Group1>[^\"]+)\"\\s*>*.*";
+		Pattern p = Pattern.compile(pattern);
 
-	     public Iterator<String> getLinks() {
-	    	 return null;
-	     }
+		try {
+			while (reader.ready()) {
+				String line = reader.readLine();
+				Matcher m = p.matcher(line);
+				if (m.find()) {
+					linkList.add(m.group("Group1"));
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Iterator<String> getLinks() {
+		return linkList.iterator();
+	}
 }
--- src/main.java
+++ src/main.java
@@ -1,8 +1,24 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Iterator;
+
+
 
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		LinkFinder finder = new LinkFinder();
+		try {
+			FileInputStream fileInputStream = new FileInputStream("src/neumont.edu");
+			finder.processPage(fileInputStream);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		Iterator<String> iterator = finder.getLinks();
+		while(iterator.hasNext()){
+			System.out.println(iterator.next());
+		}	
 
 	}
 
